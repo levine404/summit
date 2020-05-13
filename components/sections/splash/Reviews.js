fdf0da925dd941e814048da5497b5b4b57253999
levine404 <levine404@gmail.com>
2020-05-12 20:24:19 -0400

    Added FB Link

--- components/sections/splash/Reviews.js
+++ components/sections/splash/Reviews.js
@@ -28,6 +28,10 @@ const Reviews = () => {
       "We just bought a new house and of course the carpet needed cleaning! I called Mr Gilson and a time was set with my time schedule!! He was there when he said he would b and did a fantastic job on my carpet! This company is an outstanding company and I highly recommend it!"
       <LeaveAReviewButton />
       <img src={qrCodeImage} width="100" className="qr-code" />
+      <br />
+      <a href="https://www.facebook.com/summitcarpetcleaningtn/">
+        <button className="fbButton">View Our Facebook Page</button>
+      </a>
       <style jsx>{`
         #reviews {
           padding: 12px;
@@ -43,6 +47,23 @@ const Reviews = () => {
         }
         .qr-code {
           margin: 12px;
+        }
+        .fbButton {
+          width: 100%;
+          border-radius: 12px;
+          padding: 12px;
+          border: none;
+          color: #fff;
+          opacity: 0.9;
+          background: #3b5998;f
+          font-size: 1.3em;
+          font-weight: 900;
+          box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+          transition: all 0.3s;
+          cursor: pointer;
+        }
+        .fbButton:hover {
+          opacity: 1;
         }
       `}</style>
     </div>
